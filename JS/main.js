f8514e9ccd9bf4ce3f4a058e02a72df79fcf8c7e
emmyjoe98 <eat16001@byui.edu>
2019-09-21 15:42:03 -0500

    final touches

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,2 @@
 const day = {day: 'numeric', month: 'numeric', year: 'numeric'};
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', day);
-
-const time = {hour: 'numeric', minute: 'numeric', second: 'numeric'};
-document.getElementById('currenttime').textContent = new Date().toLocaleDateString('en-US', time);
