1809beb8d8f0de952fc5ed5c29ea0e184722abfc
emmyjoe98 <eat16001@byui.edu>
2019-09-21 13:56:10 -0500

    fixes to portal page

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,5 @@
-const options = {day: 'numeric', month: 'numeric', year: 'numeric'};
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+const day = {day: 'numeric', month: 'numeric', year: 'numeric'};
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', day);
+
+const time = {hour: 'numeric', minute: 'numeric', second: 'numeric'};
+document.getElementById('currenttime').textContent = new Date().toLocaleDateString('en-US', time);
