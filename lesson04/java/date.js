0a1a398c605ac99aeacd1fa746d01f70ec871a53
emmyjoe98 <eat16001@byui.edu>
2019-10-12 17:11:00 -0500

    fixes to preston

--- lesson04/java/date.js
+++ lesson04/java/date.js
@@ -1,2 +1,7 @@
 const day = {day: 'numeric', month: 'numeric', year: 'numeric'};
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', day);
+
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
